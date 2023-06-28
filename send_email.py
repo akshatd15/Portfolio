51013d6f558fbe0b570c932c71106ed78ca98e7a
DEVINx34 <devinx34@gmail.com>
2023-06-28 17:37:01 +0530

    password

--- send_email.py
+++ send_email.py
@@ -1,11 +1,12 @@
-import smtplib,ssl
+import smtplib, ssl
+import os
 
 
 def send_mail(message):
     host = "smtp.gmail.com"
     port = 465
     username = "devinx34@gmail.com"
-    password = "qipmigkeugisekzy"
+    password = os.getenv("PASSWORD")
     receiver = "devinx34@gmail.com"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
