7facd8c9ba853a4afbdea4736dabea6d71fa6855
DEVINx34 <devinx34@gmail.com>
2023-07-03 22:21:35 +0530

    update

--- Home.py
+++ Home.py
@@ -4,63 +4,59 @@ import pandas
 st.session_state['answer'] = 'NLKnaP/ab5Tdo5OITHuNwWt6mMQG4XIVOEbHhAdhZpg'
 
 if st.session_state['answer'] in realans:
-    answerStat = "correct"
+    st.set_page_config(layout="wide")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(r"images\main_photo.jpg")
+
+    with col2:
+        st.title("Akshat Baliyan")
+        content = """Hi, I am Akshat Baliyan! I am a advanced python programmer , currently leaning ML 
+        and pursuing to be an AI engineer!
+        """
+        st.info(content)
+
+    st.write("")
+    st.write("Below you can find some of the appsI have built in Python. Feel free to contact me!")
+    st.write("")
+
+    col3, empty_col, col4 = st.columns([2, 1, 2])
+    df = pandas.read_csv("data.csv", sep=";")
+
+    with col3:
+        for index, row in df[1:5].iterrows():
+            st.header(row["title"])
+            st.write(row["description"])
+            img = row["image"]
+            st.image(f"images\{img}")
+            link = row["url"]
+            st.write(f"[Source Code]({link})")
+
+    with col4:
+        for index, row in df[5:8].iterrows():
+            st.header(row["title"])
+            st.write(row["description"])
+            img = row["image"]
+            st.image(f"images\{img}")
+            link = row["url"]
+            st.write(f"[Source Code]({link})")
+
+    col5, empty_col_2, col6 = st.columns([2, 1, 2])
+
+    st.write("")
+    st.write("")
+    st.write("")
+
+    with col5:
+        st.header("Ongoing")
+        for index, row in df[8:9].iterrows():
+            st.header(row["title"])
+            st.write(row["description"])
+            img = row["image"]
+            st.image(f"images\{img}")
+            link = row["url"]
+            st.write(f"[Source Code]({link})")
 elif st.session_state['answer'] not in realans:
     answerStat = "incorrect"
 
-st.set_page_config(layout="wide")
-
-col1, col2 = st.columns(2)
-with col1:
-    st.image(r"images\main_photo.jpg")
-
-with col2:
-    st.title("Akshat Baliyan")
-    content = """Hi, I am Akshat Baliyan! I am a advanced python programmer , currently leaning ML 
-    and pursuing to be an AI engineer!
-    """
-    st.info(content)
-
-
-st.write("")
-st.write("Below you can find some of the appsI have built in Python. Feel free to contact me!")
-st.write("")
-
-col3, empty_col, col4 = st.columns([2,1,2])
-df = pandas.read_csv("data.csv",sep=";")
-
-
-with col3:
-    for index, row in df[1:5].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        img = row["image"]
-        st.image(f"images\{img}")
-        link=row["url"]
-        st.write(f"[Source Code]({link})")
-
-with col4:
-    for index, row in df[5:8].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        img = row["image"]
-        st.image(f"images\{img}")
-        link=row["url"]
-        st.write(f"[Source Code]({link})")
-
-col5, empty_col_2, col6 = st.columns([2,1,2])
-
-
-st.write("")
-st.write("")
-st.write("")
-
-with col5:
-    st.header("Ongoing")
-    for index, row in df[8:9].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        img = row["image"]
-        st.image(f"images\{img}")
-        link=row["url"]
-        st.write(f"[Source Code]({link})")
