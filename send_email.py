6d9bcddbf1189c79ae0fbad151c0563a82c6e000
DEVINx34 <devinx34@gmail.com>
2023-09-27 10:41:51 +0530

    update

--- send_email.py
+++ send_email.py
@@ -6,7 +6,7 @@ def send_mail(message):
     host = "smtp.gmail.com"
     port = 465
     username = "devinx34@gmail.com"
-    password = os.getenv("PASSWORD")
+    password = "qipmigkeugisekzy"
     receiver = "devinx34@gmail.com"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
