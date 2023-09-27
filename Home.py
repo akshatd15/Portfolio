fba9e4444858a652f8b7bdf8392879cca77ed531
DEVINx34 <devinx34@gmail.com>
2023-09-27 10:28:02 +0530

    update

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 with col1:
-    st.image(r"images\main_photo.jpg")
+    st.image(r"images/main_photo.jpg")
 
 with col2:
     st.title("Akshat Baliyan")
